ec0dc7c276daae7f618359058329d81a46f882d2
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-10-04 02:41:47 -0300

    Add files via upload

---
+++ Exercicios de interpretação - Gustavo Mazui.js
@@ -0,0 +1,40 @@
+//////////////////////Exercicio de Interpretação 1 
+
+/*let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)*/ 
+
+//O código soma os números de 0 a 4 e armazena o resultado na variável valor. Depois, imprime o valor final (10) no console.
+
+
+
+//////////////////////Exercicio de Interpretação 2
+
+/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+} */ 
+
+//a) Os números maiores que 18:
+//b) Usando somente o for, para acessar cada indice       
+
+
+
+//////////////////////Exercicio de Interpretação 3
+
+/* const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+} /* 
+
+//Será Impresso linhas com asteriscos, começando com 1 e indo até 4 asteriscos. */
