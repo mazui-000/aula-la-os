5574d3fc50b3f301758bc3e53bf726d013b2ba24
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-10-04 11:42:14 -0300

    Update Exercicios de escrita - Gustavo Mazui.js

--- Exercicios de escrita - Gustavo Mazui.js
+++ Exercicios de escrita - Gustavo Mazui.js
@@ -17,3 +17,44 @@ console.log(nomesPets);
 } */ 
 
 //////////////////////Exercicio de Escrita 2 
+
+let arrayOriginal = [54,34,39,23,90,21,17]
+    for(let numero of arrayOriginal){
+        console.log (numero)
+    }
+    
+
+//divindo por 10
+    function arrayzada (arrayOriginal) {
+       for (let i = 0; i < arrayOriginal.length; i++) {
+        console.log(arrayOriginal[i]/10)
+    }
+    }
+console.log ("divididos por 10:")
+    arrayzada(arrayOriginal)
+
+
+    //buscando por pares
+    function arrayPares (array) {
+        let numerosPares = []; 
+    for (let i=0; i < array.length;i++){
+    if (array [i]%2===0){
+        numerosPares.push(array[i]);
+        }
+    }
+    console.log ("Numeros Pares:")
+    console.log(numerosPares);
+    }
+    arrayPares(arrayOriginal)
+    
+
+
+    //D
+    function novaArrayStrings (Array) {
+    let newArray = [];
+    for (let i = 0; i < arrayOriginal.length; i++){
+        newArray.push(" O elemento do index "+ i + " é: " + arrayOriginal[i] );
+    }
+    console.log(newArray); 
+    }
+    novaArrayStrings(arrayOriginal)
