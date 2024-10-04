35c6b294b96f978f23f34dc6c69915b8c2e63605
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-10-04 02:43:57 -0300

    Add files via upload

---
+++ Exercicios de escrita - Gustavo Mazui.js
@@ -0,0 +1,19 @@
+//////////////////////Exercicio de Escrita 1 
+
+/* let quantidadePets = prompt(Number("Quantos bichinhos de estimação você tem?"));
+
+if (quantidadePets === 0) {
+    alert("Que pena! Você pode adotar um pet!");
+    
+} else {
+    var nomesPets = [];
+    for (var i = 0; i < quantidadePets; i++) {
+        var nome = prompt("Digite o nome do seu pet " + (i + 1) + ":");
+        nomesPets.push(nome);
+    }
+    alert("Os nomes dos seus pets são: " + nomesPets.join(", "));
+
+console.log(nomesPets);
+} */ 
+
+//////////////////////Exercicio de Escrita 2 
